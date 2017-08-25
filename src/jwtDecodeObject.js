bf0562a738b0f498f819b88a89d657cd6fb11f85
cdowney <cdowney@cimpress.com>
2017-08-25 09:52:28 -0400

    FN-639 add comment explaining seemingly unuseful module

--- src/jwtDecodeObject.js
+++ src/jwtDecodeObject.js
@@ -1,5 +1,11 @@
 const jwtDecode = require('jwt-decode');
 
+/**
+ * Due to the way the Javascript module framework works,
+ * sinon is unable to mock the jwt-decode library directly.
+ * Since this breaks our tests irreparably, it's easiest just to export
+ * an object that exposes an API similar to how we used jsonwebtoken
+ */
 module.exports = {
   decode: jwtDecode,
 };
